b88abe270e4efd2345c0ad5503221d14245024a4
Joseph Acevedo <joseph.acevedo.mail@gmail.com>
2016-03-30 15:02:30 -0500

    This is just getting started.

---
+++ public/main.php
@@ -0,0 +1,31 @@
+<?php
+
+$hello = "Hello World!";
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Konbini, The World's Premire Online Store for all things Japanese</title>
+	<link rel="stylesheet" href="/css/main.css">
+</head>
+<body>
+	<header id="mainnav">
+		<div id="navright">
+			<div id="titlestuff">
+				<img id="sitetitleimage" src="/img/site/IMG_1029.jpg">
+				<div id="sitetitle" class="font1 fontlarge fontcenter">Konbini</div>
+			</div>
+		</div>
+		<div id="navbar">
+			<div id="toplinkscontainer" class="font1 fontmedium">
+				<div>Categories</div>
+				<div>Stuff</div>
+				<div>Blog</div>
+			</div>
+		</div>
+	</header>
+	<main>
+	</main>
+</body>
+</html>
